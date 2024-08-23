63f3552e15e5e7f1097934446c44e12d7d6b19a4
Jan Bachmann <bachmann@csh.ac.at>
2024-08-23 10:17:47 +0200

    Add documentation

--- patch_workshop/stats.py
+++ patch_workshop/stats.py
@@ -3,5 +3,17 @@ from typing import Union, List, Any, Tuple
 import numpy as np
 
 def compute_cdf(a_vals: Union[List[Any], np.ndarray]) -> Tuple[np.ndarray, np.ndarray]:
+    """Computes the cumulative distribution function of the given values.
+
+    Parameters
+    ----------
+    a_vals : Union[List[Any], np.ndarray]
+        A list of values to compute the CDF for.
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        A tuple of two equal-length arrays, the first containing the sorted values and the second the CDF.
+    """
     a_sort = np.sort(a_vals)
     return a_sort, (np.arange(len(a_sort)) / len(a_sort))
